657f2c3629d060b6463098d38bc98c5c79552e27
Alexia Dormann <alexia.dormann@epfl.ch>
2024-04-11 10:19:18 +0200

    Add data processing scripts

--- src/create_bw_cell_type.py
+++ src/create_bw_cell_type.py
@@ -1,39 +1,50 @@
 import anndata
 import subprocess
 import glob
+import os 
 
 from data_preprocessing import preprocess_data
 
-NAME_DATASET =['D8_1','D8_2','D12_1','D12_2','D20_1', 'D20_2', 'D22_1', 'D22_2']
+#NAME_DATASET =['D8_1','D8_2','D12_1','D12_2','D20_1', 'D20_2', 'D22_1', 'D22_2']
+NAME_DATASET = ['D20_1']
 
 #Load concatenated data
-adata = preprocess_data('../filtered_features/', '../results/cell_types.csv','../results/concat.h5ad')
+#adata = preprocess_data('../data/initial_10x_outputs/filtered_features/', '../results/cell_types.csv','../results/concat.h5ad')
 adata = anndata.read_h5ad('../results/concat.h5ad')
 
-""" for d in NAME_DATASET:
+for d in NAME_DATASET:
 
-    subprocess.run('mkdir ../results/bam_cell_type/' + d)
+    if not os.path.exists('../results/bam_cell_type/' + d):
+        os.makedirs('../results/bam_cell_type/' + d)
 
     #Create file with cell barcodes and group 
     DX =  adata[adata.obs.batch == d]
 
     barcodes = DX.obs.cell_type
+    barcodes = barcodes.cat.rename_categories({'Myoblast/Myocytes': 'Myoblast'})
     barcodes.index = [b[:-2] for b in barcodes.index.values]
     barcodes.to_csv('../results/bam_cell_type/' + d + '/' + d + '_cell_types.tsv', header=False, sep='\t')
 
     #Split the bam by cell type
-    sinto_command = ('sinto filterbarcodes -b ../data/initial_10x_outputs/atac_peaks/' + 
+    sinto_command = ('sinto filterbarcodes -p 8 -b ../data/initial_10x_outputs/atac_peaks/' + 
                  d +'_ATAC.bam -c ../results/bam_cell_type/' + d +
                  '/' + d + '_cell_types.tsv --outdir ../results/bam_cell_type/' +
                  d + '/')
     
-    subprocess.run(sinto_command)
+    #subprocess.run(sinto_command, shell=True)
 
     #Create ATAC tracks using splitted files
     splitted_files = glob.glob('../results/bam_cell_type/' + d +'/*.bam')
 
     for f in splitted_files:
-        bamCoverage_command = ('bamCoverage --b ' + f  + 
-                               ' -o ' + f[:-3] + '.bw')
-        
-        subprocess.run(bamCoverage_command) """
+
+        #Create index file first
+        samtools_command = ('samtools index ' + f + 
+                            ' ' + f + '.bai')
+            
+        subprocess.run(samtools_command, shell=True)
+
+        bamCoverage_command = ('bamCoverage -p 8 -b ' + f  + 
+                            ' -o ' + f[:-3] + 'bw')
+            
+        subprocess.run(bamCoverage_command, shell=True)
--- src/data_preprocessing.py
+++ src/data_preprocessing.py
@@ -1,10 +1,14 @@
 import pandas as pd
 import numpy as np
-import anndata
 import gzip
 
+import anndata
 import scanpy as sc
 import pyfaidx
+import pyBigWig
+
+import torch
+from torch.utils.data import Dataset, DataLoader
 
 NAME_DATASET = ['D8_1','D8_2','D12_1','D12_2','D20_1', 'D20_2', 'D22_1', 'D22_2']
 
@@ -53,7 +57,8 @@ def preprocess_data(data_path, cell_type_path, save_path, name_datasets=NAME_DAT
     adata.obs['cell_type'] = cell_types.cell_type[adata.obs_names]
 
     #Rename batch number to be name dataset
-    adata.obs.batch.rename_categories(NAME_DATASET)
+    adata.obs.batch = adata.obs.batch.cat.rename_categories(NAME_DATASET)
+
     adata.write(save_path)
 
     return adata
@@ -71,13 +76,110 @@ def pseudo_bulk(adata, col):
         var=adata.var,
         obs=pd.DataFrame(index=indicator.columns))
 
-def fetch_sequence(adata, path_genome):
+def fetch_sequence(adata, path_genome, bp_around=0):
     
     genome = pyfaidx.Fasta(path_genome)
 
-    #Remove some chromosome
-    adata = adata[np.logical_or(np.logical_or(adata.obs.chr.str.isnumeric(), adata.obs.chr == 'X'), adata.obs.chr == 'Y')]
+    adata.var['chr'] = adata.var_names.to_series().str.split(':', n=1).str.get(0)
+    adata.var['start'] = adata.var_names.to_series().apply(lambda st: st[st.find(":")+1:st.find("-")]).astype('int')
+    adata.var['end'] = adata.var_names.to_series().str.split('-', n=1).str.get(1).astype('int')
+
+    #Remove scaffold chromosomes
+    adata = adata[:,np.logical_or(np.logical_or(adata.var.chr.str.isnumeric(), adata.var.chr == 'X'), adata.var.chr == 'Y')]
 
     #Fetch sequence on reference genome using location of peaks
-    adata.obs['chr'] = 'chr' + adata.obs['chr']
-    adata.obs['sequence'] = adata.obs.apply(lambda x: (genome[x['chr']][x['start']:x['end']]).seq, axis=1)
+    adata.var.chr = 'chr' + adata.var.chr
+    
+    adata.var['region_start'] = adata.var.start - bp_around
+    adata.var.loc[adata.var.region_start < 0,'region_start'] = 0
+    adata.var['region_end'] = adata.var.end + bp_around
+    
+    adata.var['sequence'] = adata.var.apply(lambda x: (genome[x['chr']][x['region_start']:x['region_end']]).seq, axis=1)
+
+    return adata
+
+def one_hot_encode(seq):
+    mapping = dict(zip("ACGT", range(4)))    
+    seq2 = [mapping[i] for i in seq]
+    return np.eye(4)[seq2]
+
+def encode_sequence(adata):
+    
+    #Capitalize all sequence
+    adata.var.sequence = adata.var.sequence.str.upper()
+    
+    #One hot encode the the sequence
+    return [one_hot_encode(seq) for seq in adata.var.sequence]
+
+def get_continous_track(peak_metadata):
+
+    for name, group in peak_metadata.groupby(['cell_type', 'dataset']):
+        print(name)
+        bw_file = '../results/bam_cell_type/' + name[0]
+        
+        bw = pyBigWig.open('../results/bam_cell_type/D8_1/Somite.bw')
+        bw.values("10", 100004794, 100005512)
+
+
+
+
+class SequenceDataset(Dataset):
+    """Genomic sequence pytorch dataset with discrete ATAC signal"""
+
+    def __init__(self, h5ad_file, path_ref_genome):
+        """
+        Arguments:
+            h5ad_file (string): Path to the ATAC Anndata object 
+            path_ref_genome (string): Path to the reference genome used to fetch sequence
+        """
+        self.ATAC_count_matrix = anndata.read_h5ad(h5ad_file)
+        self.path_ref_genome = path_ref_genome
+
+        #Create pseudo bulk  by cell types + time point
+        self.ATAC_count_matrix.obs['cell_type_batch'] = self.ATAC_count_matrix.obs.cell_type.astype('str') + self.ATAC_count_matrix.obs.batch.astype('str')
+        self.ATAC_count_matrix = pseudo_bulk(self.ATAC_count_matrix,'cell_type_batch')
+
+        #Normalize the aggregated count matrix
+
+
+        #Get sequence for each of the peaks in count matrix and one-hot encode it
+        self.ATAC_count_matrix = fetch_sequence(self.ATAC_count_matrix, path_genome=self.path_ref_genome)
+        #self.ATAC_count_matrix.var.encoded_seq = encode_sequence(self.ATAC_count_matrix)
+
+        #Each data point is peak with time, cell type, and aggregated ATAC signal
+        self.sequence_ATAC = pd.DataFrame(self.ATAC_count_matrix.X, index=self.ATAC_count_matrix.obs_names, 
+                            columns=self.ATAC_count_matrix.var_names)
+
+        self.sequence_ATAC = self.sequence_ATAC.transpose()
+
+        #Change cell type + time id so that we can use pandas wide_to_long function
+        batch_dict = dict(zip(self.sequence_ATAC.columns.values, ['batch' + str(x) for x in np.arange(len(self.sequence_ATAC.columns.values))]))
+        convert_back = dict(zip(np.arange(len(self.sequence_ATAC.columns.values)), self.sequence_ATAC.columns.values))
+
+        self.sequence_ATAC = self.sequence_ATAC.rename(columns=batch_dict)
+        self.sequence_ATAC["peakID"] = self.sequence_ATAC.index
+
+        self.sequence_ATAC = pd.wide_to_long(self.sequence_ATAC, stubnames='batch', i='peakID', j='ATAC')
+
+        self.sequence_ATAC = self.sequence_ATAC.reset_index()
+        self.sequence_ATAC.ATAC = [convert_back[x] for x in self.sequence_ATAC.ATAC]
+
+        self.sequence_ATAC['cell_type'] = [x.split('D')[0] for x in self.sequence_ATAC.ATAC]
+        self.sequence_ATAC['dataset'] = ['D' + x.split('D')[1] for x in self.sequence_ATAC.ATAC]
+
+        self.sequence_ATAC = self.sequence_ATAC.drop(columns=['ATAC'])
+        self.sequence_ATAC = self.sequence_ATAC.rename(columns={'batch':'ATAC'})
+
+        #Add sequence for each peak
+
+    def __len__(self):
+        return len(self.sequence_ATAC.shape[0])
+
+    def __getitem__(self, idx):
+
+        return self.sequence_ATAC[idx]
+
+
+
+
+    
