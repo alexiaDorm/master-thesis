7671e7e1e2d4834fe38d2a29ddacd071d59a5076
Alexia Dormann <alexia.dormann@epfl.ch>
2024-04-15 09:08:00 +0200

    Update create_bw_cell_type.py

--- src/create_bw_cell_type.py
+++ src/create_bw_cell_type.py
@@ -4,7 +4,6 @@ import glob
 import os 
 
 NAME_DATASET =['D8_1','D8_2','D12_1','D12_2','D20_1', 'D20_2', 'D22_1', 'D22_2']
-NAME_DATASET =['D22_1', 'D22_2']
 
 #Load concatenated data
 adata = anndata.read_h5ad('../results/concat.h5ad')
@@ -25,15 +24,12 @@ for d in NAME_DATASET:
     #Split the bam by cell type
     ATAC_bam = '../../../../../projects/schuelke-cubi-muscle-dev/work/BtE_P07_P08_analyses/MULTIOME/outputs/' + d[0:-1] + 'REP' + d[-1] + '_run1/outs/atac_possorted_bam.bam'
 
-    ATAC_bam = '../../../../../projects/schuelke-cubi-muscle-dev/work/BtE_P07_P08_analyses/MULTIOME/outputs/' + d[0:-2] + '-15_REP' + d[-1] + '_run1/outs/atac_possorted_bam.bam'
-
-
     sinto_command = ('sinto filterbarcodes -p 8 -b ' + ATAC_bam + 
                  ' -c ../results/bam_cell_type/' + d +
                  '/' + d + '_cell_types.tsv --outdir ../results/bam_cell_type/' +
                  d + '/')
     
-    subprocess.run(sinto_command, shell=True)
+    #subprocess.run(sinto_command, shell=True)
 
     #Create ATAC tracks using splitted files
     splitted_files = glob.glob('../results/bam_cell_type/' + d +'/*.bam')
@@ -49,4 +45,4 @@ for d in NAME_DATASET:
         bamCoverage_command = ('bamCoverage -p 8 -b ' + f  + 
                             ' -o ' + f[:-3] + 'bw --binSize 1' )
             
-        #subprocess.run(bamCoverage_command, shell=True)
+        subprocess.run(bamCoverage_command, shell=True)
