56407ec98a72eca11b0c8d1f072356543f759ccd
Alexia Dormann <alexia.dormann@epfl.ch>
2024-04-16 12:48:38 +0200

    Add coverage distribution code

--- src/compute_coverage.py
+++ src/compute_coverage.py
@@ -2,6 +2,7 @@
 
 import glob
 import subprocess
+import pyBigWig
 
 NAME_DATASET =['D8_1','D8_2','D12_1','D12_2','D20_1', 'D20_2', 'D22_1', 'D22_2']
 
@@ -12,6 +13,10 @@ for d in NAME_DATASET:
     for f in splitted_files:
         samtools_command = "samtools depth -a " + f +  " |  awk '{sum+=$3} END { print sum/NR}'"
         
-        print(f)
-        subprocess.run(samtools_command, shell=True)
+        #subprocess.run(samtools_command, shell=True)
+
+        bamCoverage_command = ('bamCoverage -p 8 -b ' + f  + 
+                            ' -o ' + f[:-4] + '_cov.bw --binSize 10000' )
+            
+        subprocess.run(bamCoverage_command, shell=True)
 
