21ef8a8d20459245477f496c6b12e70f9c96bc26
Alexia Dormann <alexia.dormann@epfl.ch>
2024-04-24 11:53:37 +0200

    Update create_peak_set.py

--- src/data_processing/create_peak_set.py
+++ src/data_processing/create_peak_set.py
@@ -20,31 +20,31 @@ for t in TIME_POINT:
     peaks_rep2 = peaks_rep2[np.logical_or(np.logical_or(peaks_rep2[0].str.isnumeric(), peaks_rep2[0] == 'X'), peaks_rep2[0] == 'Y')]
 
     #Merge overlapping peaks
-    if not os.path.exists('../../results/tmp/'):
-        os.makedirs('../../results/tmp/')
+    if not os.path.exists('../results/tmp/'):
+        os.makedirs('../results/tmp/')
 
     peaks = pd.concat([peaks_rep1, peaks_rep2])
-    peaks_path = "../../results/tmp/peaks.bed"
+    peaks_path = "../results/tmp/peaks.bed"
     peaks.to_csv(peaks_path, header=False, index=False, sep='\t')
     
-    sort_bed = "sortBed -i " + peaks_path + " > ../../results/tmp/peaks_sorted.bed"
+    sort_bed = "sortBed -i " + peaks_path + " > ../results/tmp/peaks_sorted.bed"
     subprocess.run(sort_bed, shell=True)
 
-    merge_bedtools = "bedtools merge -i ../../results/tmp/peaks_sorted.bed -c 1 -o count > ../../results/tmp/count_peaks.bed"
+    merge_bedtools = "bedtools merge -i ../results/tmp/peaks_sorted.bed -c 1 -o count > ../results/tmp/count_peaks.bed"
     subprocess.run(merge_bedtools, shell=True)
     
     #Only keep peaks that were in both replicates
-    peaks = pd.read_csv("../../results/tmp/count_peaks.bed" ,header=None, sep='\t')
+    peaks = pd.read_csv("../results/tmp/count_peaks.bed" ,header=None, sep='\t', low_memory=False)
     peaks = peaks[peaks[3] == 2]
 
     peaks_per_rep.append(peaks.iloc[:,0:3])
 
 #Take union of peaks of all time points and merge overlapping peaks 
 peaks = pd.concat(peaks_per_rep)
-peaks.to_csv("../../results/tmp/all_peaks.bed", header=False, index=False, sep='\t')
+peaks.to_csv("../results/tmp/all_peaks.bed", header=False, index=False, sep='\t')
 
-sort_bed = "sortBed -i ../../results/tmp/all_peaks.bed > ../../results/tmp/all_peaks_sorted.bed"
+sort_bed = "sortBed -i ../results/tmp/all_peaks.bed > ../results/tmp/all_peaks_sorted.bed"
 subprocess.run(sort_bed, shell=True)
 
-merge_bedtools = "bedtools merge -i ../../results/tmp/all_peaks_sorted.bed > ../../results/common_peaks.bed"
+merge_bedtools = "bedtools merge -i ../results/tmp/all_peaks_sorted.bed > ../results/common_peaks.bed"
 subprocess.run(merge_bedtools, shell=True)
