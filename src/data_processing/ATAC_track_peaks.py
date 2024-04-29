263ddf2f8fd50998432107ae92be5879e77f63f7
Alexia Dormann <alexia.dormann@epfl.ch>
2024-04-29 09:23:20 +0200

    Update ATAC_track_peaks.py

--- src/data_processing/ATAC_track_peaks.py
+++ src/data_processing/ATAC_track_peaks.py
@@ -9,7 +9,7 @@ from utils_data_preprocessing import get_continuous_ATAC_peaks
 
 TIME_POINT = ["D8", "D12", "D20", "D22-15"]
 
-with open('../results/peaks_seq.pkl', 'rb') as file:
+""" with open('../results/peaks_seq.pkl', 'rb') as file:
     peaks = pickle.load(file)
 
 peaks['middle'] = np.round((peaks.end - peaks.start)/2 + peaks.start).astype('uint32')
@@ -32,25 +32,26 @@ for d in TIME_POINT:
         with open(("../results/ATAC/" + f.removeprefix("../results/bam_cell_type/").removesuffix(".bw") + ".pkl"), 'wb') as file:
             pickle.dump(ATAC, file)
 
-        del ATAC
+        del ATAC """
 
 #Merge all datasets into one adding columns: time + cell type 
 pkl_files = glob.glob('../results/ATAC/*/*.pkl')
 
-with open(pkl_files[0], 'rb') as file:
-    ATAC = pd.DataFrame(pickle.load(file))
-
-ATAC['time'] = [pkl_files[0].split('/')[3]] * ATAC.shape[0]
-ATAC['cell_type'] = [pkl_files[0].split('/')[4].removesuffix('.pkl')] * ATAC.shape[0]
-
 for f in pkl_files[1:]:
     with open(f, 'rb') as file:
         tmp = pd.DataFrame(pickle.load(file))
-    
-    tmp['time'] = ([f.split('/')[3]] * tmp.shape[0]).astype('category')
-    tmp['cell_type'] = ([f.split('/')[4].removesuffix('.pkl')] * tmp.shape[0]).astype('category')
 
-    ATAC = pd.concat([ATAC, tmp])
+    tmp['time'] = [f.split('/')[3]] * tmp.shape[0]
+    tmp.time = tmp.time.astype('category')
+        
+    tmp['cell_type'] = ([f.split('/')[4].removesuffix('.pkl')] * tmp.shape[0])
+    tmp.cell_type = tmp.cell_type.astype('category')
 
+    with open(f, 'wb') as file:
+            pickle.dump(tmp, file)
+
+    del tmp
+
+ATAC = pd.concat(pd.read_pickle(f) for f in pkl_files)
 with open('../results/ATAC_peaks.pkl', 'wb') as file:
             pickle.dump(ATAC, file)
