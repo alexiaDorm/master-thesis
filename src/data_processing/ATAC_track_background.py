64e75b6c3d5a632efd81d39924b156eb139942e0
Alexia Dormann <alexia.dormann@epfl.ch>
2024-04-29 09:47:20 +0200

    Update ATAC_track_background.py

--- src/data_processing/ATAC_track_background.py
+++ src/data_processing/ATAC_track_background.py
@@ -9,7 +9,7 @@ from utils_data_preprocessing import get_continuous_ATAC_background
 
 NAME_DATASET = ["D8", "D12", "D20", "D22-15"]
 
-with open('../results/background_GC_matched.pkl', 'rb') as file:
+""" with open('../results/background_GC_matched.pkl', 'rb') as file:
     background = pickle.load(file)
 
 for d in NAME_DATASET:
@@ -29,25 +29,26 @@ for d in NAME_DATASET:
         with open(('../results/background/' + f.removeprefix("../results/bam_cell_type/").removesuffix(".bw") + ".pkl"), 'wb') as file:
             pickle.dump(ATAC, file)
 
-        del ATAC
+        del ATAC """
 
 #Merge all datasets into one adding columns: time + cell type 
 pkl_files = glob.glob('../results/background/*/*.pkl')
 
-with open(pkl_files[0], 'rb') as file:
-    ATAC = pd.DataFrame(pickle.load(file))
-
-ATAC['time'] = [pkl_files[0].split('/')[3]] * ATAC.shape[0]
-ATAC['cell_type'] = [pkl_files[0].split('/')[4].removesuffix('.pkl')] * ATAC.shape[0]
-
-for f in pkl_files[1:]:
+for f in pkl_files:
     with open(f, 'rb') as file:
         tmp = pd.DataFrame(pickle.load(file))
-    
+
     tmp['time'] = [f.split('/')[3]] * tmp.shape[0]
-    tmp['cell_type'] = [f.split('/')[4].removesuffix('.pkl')] * tmp.shape[0]
+    tmp.time = tmp.time.astype('category')
+        
+    tmp['cell_type'] = ([f.split('/')[4].removesuffix('.pkl')] * tmp.shape[0])
+    tmp.cell_type = tmp.cell_type.astype('category')
 
-    ATAC = pd.concat([ATAC, tmp])
+    with open(f, 'wb') as file:
+            pickle.dump(tmp, file)
 
+    del tmp
+
+ATAC = pd.concat(pd.read_pickle(f) for f in pkl_files)
 with open('../results/ATAC_background.pkl', 'wb') as file:
             pickle.dump(ATAC, file)
