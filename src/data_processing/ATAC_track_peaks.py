5a0b1bd1f5a6b15322d02f6910a2936adb2bf18f
Alexia Dormann <alexia.dormann@epfl.ch>
2024-05-02 14:55:34 +0200

    Update ATAC_track_peaks.py

--- src/data_processing/ATAC_track_peaks.py
+++ src/data_processing/ATAC_track_peaks.py
@@ -37,7 +37,7 @@ for d in TIME_POINT:
 #Merge all datasets into one adding columns: time + cell type 
 pkl_files = glob.glob('../results/ATAC/*/*.pkl')
 
-for f in pkl_files:
+""" for f in pkl_files:
     with open(f, 'rb') as file:
         tmp = pd.DataFrame(pickle.load(file))
 
@@ -54,16 +54,16 @@ for f in pkl_files:
     with open(f, 'wb') as file:
             pickle.dump(tmp, file)
 
-    del tmp
+    del tmp """
 
-""" ATAC = pd.concat(pd.read_pickle(f) for f in pkl_files[:13])
+ATAC = pd.concat(pd.read_pickle(f) for f in pkl_files[:13])
 with open('../results/ATAC_peaks1.pkl', 'wb') as file:
             pickle.dump(ATAC, file)
 del ATAC
 
 ATAC = pd.concat(pd.read_pickle(f) for f in pkl_files[13:])
 with open('../results/ATAC_peaks2.pkl', 'wb') as file:
-            pickle.dump(ATAC, file) """
+            pickle.dump(ATAC, file)
 
 """ ATAC = pd.concat(pd.read_pickle(f) for f in ['../results/ATAC_peaks1.pkl', '../results/ATAC_peaks2.pkl'])
 with open('../results/ATAC_peaks.pkl', 'wb') as file:
