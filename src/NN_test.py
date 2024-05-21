b027a4c48b721d617a61fcf5ba2aeb332f2027d3
Alexia Dormann <alexia.dormann@epfl.ch>
2024-05-21 16:11:37 +0200

    Update NN_test.py

--- src/NN_test.py
+++ src/NN_test.py
@@ -75,7 +75,7 @@ def train():
                 group['lr'] = lr
 
         if epoch > 24:
-            criterion = ATACloss_alt(weight_MSE = (epoch - 25)/50 * 4)
+            criterion = ATACloss_alt(weight_MSE = (epoch - 25)/50 * 2)
         
         running_loss, epoch_steps = 0.0, 0
         running_MNLLL, running_MSE = 0.0, 0.0
