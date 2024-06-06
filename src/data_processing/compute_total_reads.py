c1cce50cb68d90df8520a4e593734ed4eef78ce0
Alexia Dormann <alexia.dormann@epfl.ch>
2024-06-06 09:40:16 +0200

    Update compute_total_reads.py

--- src/data_processing/compute_total_reads.py
+++ src/data_processing/compute_total_reads.py
@@ -9,7 +9,7 @@ NAME_DATASET =['D8','D12','D20','D22-15']
 
 for d in NAME_DATASET:
 
-    bam_files = glob.glob('../results/bam_cell_type/' + d +'/*_unstranded.bam')
+    bam_files = glob.glob('../results/bam_cell_type/' + d +'/*.bam')
 
     names, total_reads = [], []
     for f in bam_files:
@@ -18,7 +18,7 @@ for d in NAME_DATASET:
         tot = subprocess.check_output(samtools_command, shell=True)
         
         total_reads.append(tot)
-        names.append(f.removeprefix('../results/bam_cell_type/').removesuffix('_unstranded.bam'))
+        names.append(f.removeprefix('../results/bam_cell_type/').removesuffix('.bam'))
 
     total_reads = pd.DataFrame({'batch': names, 'total': total_reads})
     total_reads.to_csv('../results/bam_cell_type/' + d + '/total_reads.csv', header=None, index=False)
