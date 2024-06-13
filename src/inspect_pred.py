d9bb59346eac7d781666b62b5c392fbf39344089
Alexia Dormann <alexia.dormann@epfl.ch>
2024-06-13 11:15:45 +0200

    Create inspect_pred.py

---
+++ src/inspect_pred.py
@@ -0,0 +1,33 @@
+import torch
+from models.models import CATAC
+from data_processing.utils_data_preprocessing import one_hot_encode
+import pandas as pd
+
+path_seq = "../results/peaks_seqtest.pkl"
+path_ATAC = "../results/ATAC_peakstest.pkl"
+
+path_model = '../results/insertion/KLD_model_1e-3.pkl'
+
+#Load model
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = CATAC(nb_conv=8, nb_filters=64, first_kernel=21, 
+                      rest_kernel=3, profile_kernel_size=75, out_pred_len=1024, 
+                      nb_pred=13, nb_cell_type_CN = 0)
+        
+model.load_state_dict(torch.load(path_model, map_location=device))
+
+#Load sequence and ATAC
+ATAC = pd.Series(pd.read_pickle(path_ATAC))
+print(ATAC.head())
+
+seq = pd.Series(pd.read_pickle(path_seq)).sequence
+
+#On-hot encode the sequences
+seq = seq.apply(lambda x: one_hot_encode(x))
+seq = torch.tensor(seq).permute(0,2,1)
+
+x, profile, count = model(seq)
+profile = torch.nn.functional.softmax(profile[0])
+profile = profile * torch.exp(count[0])
+
