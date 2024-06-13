af046eb61df225988a80bd5eac1243a1564cd484
Alexia Dormann <alexia.dormann@epfl.ch>
2024-06-13 11:33:35 +0200

    Update inspect_pred.py

--- src/inspect_pred.py
+++ src/inspect_pred.py
@@ -2,13 +2,14 @@ import torch
 from models.models import CATAC
 from data_processing.utils_data_preprocessing import one_hot_encode
 import pandas as pd
+import pickle
 
 path_seq = "../results/peaks_seqtest.pkl"
 path_ATAC = "../results/ATAC_peakstest.pkl"
 
 path_model = '../results/insertion/KLD_model_1e-3.pkl'
 
-""" #Load model
+#Load model
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 model = CATAC(nb_conv=8, nb_filters=64, first_kernel=21, 
@@ -16,24 +17,25 @@ model = CATAC(nb_conv=8, nb_filters=64, first_kernel=21,
                       nb_pred=13, nb_cell_type_CN = 0)
         
 model.load_state_dict(torch.load(path_model, map_location=device))
- """
+
 #Load sequence and ATAC
 ATAC = pd.read_pickle(path_ATAC)
-seq_id = ATAC.index[0:5]
+seq_id = ATAC.sample(5).index
 
 seq = pd.read_pickle(path_seq).sequence
 
-ATAC = ATAC.loc[seq_id]
+ATAC = ATAC.loc[seq_id, 0]
 seq = seq.loc[seq_id]
 
-print(ATAC)
-print(seq)
 
 #On-hot encode the sequences
-""" seq = seq.apply(lambda x: one_hot_encode(x))
+seq = seq.apply(lambda x: one_hot_encode(x))
 seq = torch.tensor(seq).permute(0,2,1)
 
 x, profile, count = model(seq)
 profile = torch.nn.functional.softmax(profile[0])
-profile = profile * torch.exp(count[0]) """
+profile = profile * torch.exp(count[0])
 
+ATAC = pd.Dataframe({"true": ATAC, "pred": profile})
+with open('../results/pred.pkl', 'wb') as file:
+    pickle.dump(ATAC, file)
