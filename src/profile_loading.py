0d167b3c4f5ba3734109162fa4a7d179ba5741f1
Alexia Dormann <alexia.dormann@epfl.ch>
2024-06-24 16:09:27 +0200

    Update profile_loading.py

--- src/profile_loading.py
+++ src/profile_loading.py
@@ -7,8 +7,8 @@ time_order = ['D8', 'D12', 'D20', 'D22-15']
 chr_train = ['1','2','3','4','5','7','8','9','10','11','12','14','15','16','17','18','19','20','21','X','Y']
 chr_test = ['6','13','22']  
 
-train_dataset = PeaksDataset2(data_dir + 'peaks_seq.pkl', data_dir + 'background_GC_matched_sample.pkl',
-                                 data_dir + 'ATAC_peaks.pkl', data_dir + 'ATAC_background_sample.pkl', 
+train_dataset = PeaksDataset2(data_dir + 'peaks_seqtest.pkl', data_dir + 'background_GC_matchedt.pkl',
+                                 data_dir + 'ATAC_peakstest.pkl', data_dir + 'ATAC_backgroundtest.pkl', 
                                  chr_train, time_order, 20000)
 train_dataloader = DataLoader(train_dataset, 64,
                         shuffle=True, num_workers=4)
