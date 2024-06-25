24958b22537025d119648c0f15a9838aa19a91a0
Alexia Dormann <alexia.dormann@epfl.ch>
2024-06-25 14:59:57 +0200

    Update ATAC_track_peaks.py

--- src/data_processing/ATAC_track_peaks.py
+++ src/data_processing/ATAC_track_peaks.py
@@ -17,12 +17,13 @@ with open('../results/peaks_seq.pkl', 'rb') as file:
     peaks = pickle.load(file)
 
 peaks['middle'] = np.round((peaks.end - peaks.start)/2 + peaks.start).astype('uint32')
+nb_peaks = peaks.shape[0]
 
 idx_seq, c_type = [], []
 for c in c_type:
     
-    bw_files = ['../results/bam_cell_type/' + t +'/' + c '_unstranded.bw' for t in TIME_POINT]
-    ATAC_tracks, is_defined, = [], []
+    bw_files = ['../results/bam_cell_type/' + t +'/' + c  + '_unstranded.bw' for t in TIME_POINT]
+    ATAC_tracks, is_defined = [], []
     for f in bw_files:
         if os.path.isfile(f):
             
@@ -33,20 +34,24 @@ for c in c_type:
             ATAC_tracks.append(ATAC)
 
             #The tracks are defined for cell type + time point
-            is_defined.append([True]*peaks.shape[0])
+            is_defined.append([True]*nb_peaks)
 
         else: 
 
             #If cell type not defined for time point, fill with zero tracks
-            ATAC =  np.zeros((peaks.shape[0],1024))
+            ATAC =  np.zeros((nb_peaks,1024))
             ATAC_tracks.append(ATAC)
 
             #The tracks are NOT defined for cell type + time point
-            is_defined.append([False]*peaks.shape[0])
+            is_defined.append([False]*nb_peaks)
     
     #Stack the ATAC tracks and is_defined -> shape:(#peaks, 1024, #time)
     ATAC_tracks = np.stack(ATAC_tracks)
     is_defined = np.stack(is_defined)
+
+    #Keep sequence idx
+    idx_seq.append(np.arange(0,nb_peaks))
+    c_type.append([c]*nb_peaks)
     
     print(ATAC_tracks.shape)
     break
