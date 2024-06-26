160a2d68d49580d8926a975653f3db9bea4dfe7d
Alexia Dormann <alexia.dormann@epfl.ch>
2024-06-26 14:43:26 +0200

    Update profile_loading.py

--- src/profile_loading.py
+++ src/profile_loading.py
@@ -2,6 +2,7 @@ from models.pytorch_datasets import PeaksDataset2
 from torch.utils.data import DataLoader
 from timeit import default_timer as timer
 import sys
+import pickle
 
 data_dir = "../results/"
 time_order = ['D8', 'D12', 'D20', 'D22-15']
@@ -20,6 +21,26 @@ train_dataset = PeaksDataset2(data_dir + 'peaks_seq.pkl', data_dir + 'background
                                  paths_ATAC_tracks, paths_ATAC_tracks_back, 
                                  chr_train)
 print("Train", train_dataset.ATAC_track.shape)
+
+with open('../results/train_dataset.pkl', 'wb') as file:
+    pickle.dump(train_dataset, file)
+
+del train_dataset
+
+test_dataset = PeaksDataset2(data_dir + 'peaks_seq.pkl', data_dir + 'background_GC_matched_sample.pkl',
+                                 paths_ATAC_tracks, paths_ATAC_tracks_back, 
+                                 chr_test)
+print("Train", test_dataset.ATAC_track.shape)
+
+with open('../results/test_dataset.pkl', 'wb') as file:
+    pickle.dump(test_dataset, file)
+
+del test_dataset
+
+with open('../results/train_dataset.pkl', 'rb') as file:
+    train_dataset = pickle.load(file)
+with open('../results/test_dataset.pkl', 'rb') as file:
+    test_dataset = pickle.load(file)
 
 """ train_dataloader = DataLoader(train_dataset, 64,
                         shuffle=True, num_workers=4)
