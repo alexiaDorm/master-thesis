62c913e467571f90dc8038bb57c3c45e40b36daf
Alexia Dormann <alexia.dormann@epfl.ch>
2024-07-18 14:52:14 +0200

    Update train_w_bias.py

--- src/train_w_bias.py
+++ src/train_w_bias.py
@@ -85,8 +85,8 @@ def train():
     ) as prof:
         for epoch in range(0, nb_epoch):
 
-            running_loss, epoch_steps = 0.0, 0
-            running_KLD, running_MSE = [], []
+            running_loss, epoch_steps = torch.zeros(1, device=device), 0
+            running_KLD, running_MSE = torch.zeros((1,4), device=device), torch.zeros((1,4), device=device)
 
             for i, data in enumerate(train_dataloader):
                     
@@ -100,20 +100,19 @@ def train():
                     idx_skip = idx_skip.float().to(device)
                     tn5_bias = tn5_bias.to(device)
 
-                    for param in model.parameters():
-                        param.grad = None
+                    optimizer.zero_grad()
 
                     _, profile, count = model(inputs, tn5_bias)
 
                     #Compute loss for each head
-                    #loss, KLD, MSE  = criterion(tracks, profile, count, idx_skip)
+                    loss, KLD, MSE  = criterion(tracks, profile, count, idx_skip)
 
-                    #loss.backward() 
-                    #optimizer.step()
+                    loss.backward() 
+                    optimizer.step()
 
-                    #running_loss += loss.item()
-                    #running_KLD.append(KLD.detach())
-                    #running_MSE.append(MSE.detach())
+                    running_loss += loss
+                    running_KLD = torch.cat((running_KLD, KLD[None,:]))
+                    running_MSE = torch.cat((running_MSE, MSE[None,:]))
 
                     """ #print every 2000 batch the loss
                     epoch_steps += 1
