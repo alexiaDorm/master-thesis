2cd557b8fab9d6e3b504f452efc9c7827ea9eab4
Alexia Dormann <alexia.dormann@epfl.ch>
2024-07-23 14:51:41 +0200

    Update test_chromBPNet.py

--- src/test_chromBPNet.py
+++ src/test_chromBPNet.py
@@ -11,7 +11,7 @@ with open('../results/peaks_seq.pkl', 'rb') as file:
     peaks = pickle.load(file)
 
 peaks = peaks[["chr","start","end"]] 
-peaks.loc[:,"chr"] = ["chr" + x for x in peaks.chr]
+#peaks.loc[:,"chr"] = ["chr" + x for x in peaks.chr]
 peaks["name"] = "noname"; peaks["score"] = 0; peaks["strand"] = "."
 peaks["thickStart"] = peaks.start; peaks["thickEnd"] = peaks.end
 peaks["random1"] = 0; peaks["random2"] = 0; peaks["random3"] = 0
@@ -26,7 +26,7 @@ with open('../results/background_GC_matched.pkl', 'rb') as file:
     back = pickle.load(file)
 
 back = back[["chr","start","end"]] 
-back.loc[:,"chr"] = ["chr" + x for x in back.chr]
+#back.loc[:,"chr"] = ["chr" + x for x in back.chr]
 back["name"] = "noname"; back["score"] = 0; back["strand"] = "."
 back["thickStart"] = back.start; back["thickEnd"] = back.end
 back["random1"] = 0; back["random2"] = 0; back["random3"] = 0
@@ -43,10 +43,14 @@ for t in TIME_POINT:
     splitted_files = glob.glob('../results/bam_cell_type/' + t + '/*.bam')
     for f in splitted_files:
 
+        print("Removing scaffold chromosomes")
+        cmd_remove_scaff = "samtools view -b " + f + " $(echo {1..22} X Y)  > ../results/bam_cell_type/output.bam"
+        subprocess.run(cmd_remove_scaff, shell=True)
+
         cmd_chromBPnet = 'chrombpnet pipeline \
-        -ibam ' + f + ' \
+        -ibam ../results/bam_cell_type/output.bam \
         -d "ATAC" \
-        -g ../data/hg38.fa \
+        -g ../data/hg38_nochr.fa \
         -c ' + chrom_sizes_file + ' \
         -p ../results/peaks.bed \
         -n ../results/neg_reg.bed \
