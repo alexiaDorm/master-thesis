8a30020c414a69658b91bd26ffcfe1295931395a
Alexia Dormann <alexia.dormann@epfl.ch>
2024-07-29 21:30:04 +0200

    Add traning wobias

--- src/models/models.py
+++ src/models/models.py
@@ -576,3 +576,130 @@ class CATAC_w_bias(nn.Module):
             pred_counts[i] = c(torch.cat((count, total_bias), 1)).squeeze()
 
         return x, pred_profiles.permute(1,2,0), pred_counts.permute(1,0)
+
+class CATAC_wo_bias(nn.Module):
+    def __init__(self, nb_conv=8, nb_filters=64, first_kernel=21, rest_kernel=3, out_pred_len=1024, nb_pred=4, size_final_conv=3568):
+
+        super().__init__()
+        """ Main model with cell type token
+        
+        Parameters
+        -----------
+        nb_conv: int (default 8)
+            number of convolutional layers
+            
+        nb_filters: int (default 64)
+            number of filters in the convolutional layers
+
+        first_kernel: int (default 25)
+            size of the kernel in the first convolutional layer
+
+        rest_kernel: int (default 3)
+            size of the kernel in all convolutional layers except the first one
+
+        out_pred_len: int (default 1024)
+            number of bp for which ATAC signal is predicted
+        
+        nb_pred: int (default 4)
+            number of ATAC tracks to predict
+
+        profile_conv: bool (default False)
+            if convolution for profile prediction, if not adaptative pooling and dense linear layer instead
+
+        Model Architecture 
+        ------------------------
+
+        - Body: sequence of convolutional layers with residual skip connections, dilated convolutions, 
+        and  ReLU activation functions
+
+        - # pseudo_bulk x Head : 
+            > Profile prediction head: a multinomial probability of Tn5 insertion counts at each position 
+            in the input sequence, deconvolution layer
+            > Total count prediction: the total Tn5 insertion counts over the input region, global average
+            poooling and linear layer predicting the total count per strand
+        
+        The predicted (expected) count at a specific position is a multiplication of the predicted total 
+        counts and the multinomial probability at that position.
+
+        -------------------------
+        
+        """
+        
+        #Define parameters
+        self.nb_conv = nb_conv
+        self.nb_filters = nb_filters
+        self.first_kernel = first_kernel
+        self.rest_kernel = rest_kernel
+        self.out_pred_len = out_pred_len
+        self.nb_pred = nb_pred
+        self.size_final_conv = size_final_conv
+
+        #Convolutional layers
+        self.convlayers = nn.ModuleList()
+
+        self.convlayers.append(nn.Sequential(nn.Conv1d(in_channels=11, out_channels=self.nb_filters,kernel_size=self.first_kernel),
+            nn.ReLU()))
+        
+        for i in range (1,self.nb_conv):
+            self.convlayers.append(nn.Sequential(
+                nn.Conv1d(in_channels=self.nb_filters, out_channels=self.nb_filters, kernel_size=self.rest_kernel, dilation=2**i),
+                nn.ReLU()
+                ))
+        
+        #Profile prediction heads
+        self.profile_global_pool = nn.AdaptiveAvgPool1d(1)
+
+        self.profile_heads = nn.ModuleList() 
+        for i in range(self.nb_pred):
+            self.profile_heads.append(nn.Linear(self.size_final_conv, self.out_pred_len))
+            #self.profile_heads.append(nn.Conv1d(self.nb_filters + 1, 1, kernel_size=75))
+
+
+        #Total count prediction heads
+        self.count_global_pool = nn.AdaptiveAvgPool1d(1)
+
+        self.count_heads = nn.ModuleList()
+        for i in range(self.nb_pred):
+            self.count_heads.append(nn.Linear(self.nb_filters,1))
+        
+    def forward(self, x):
+        
+        #Residual + Dilated convolution layers
+        #-----------------------------------------------
+        x = self.convlayers[0](x)
+
+        for layer in self.convlayers[1:]:
+            
+            conv_x = layer(x)
+
+            #Crop output previous layer to size of current 
+            cropsize = (x.size(2) - conv_x.size(2)) // 2
+
+            #Skipped connection
+            x = conv_x + x[:, :, cropsize:-cropsize]   
+    
+        #Profile head
+        #-----------------------------------------------
+
+        pred_profiles = torch.empty((self.nb_pred, x.size(0), self.out_pred_len), device = x.device, dtype=torch.float32)
+        for i, p in enumerate(self.profile_heads):
+                
+            #Apply global average poolling
+            profile = self.profile_global_pool(x.permute(0,2,1)) 
+            profile = profile.squeeze()
+
+            #Apply linear layer
+            pred_profiles[i] = p(profile)
+        
+        #Total count head
+        #-----------------------------------------------
+        pred_counts = torch.empty((self.nb_pred, x.size(0)), device = x.device, dtype=torch.float32)
+        for i, c in enumerate(self.count_heads):
+            
+            #Apply global average poolling
+            count = self.count_global_pool(x).squeeze()
+
+            #Concatenate total tn5 bias, Apply linear layer
+            pred_counts[i] = c(count).squeeze()
+
+        return x, pred_profiles.permute(1,2,0), pred_counts.permute(1,0)
--- src/models/pytorch_datasets.py
+++ src/models/pytorch_datasets.py
@@ -404,4 +404,126 @@ class PeaksDataset_w_bias(Dataset):
 
         return input, tracks, is_defined, tn5_bias
 
+class PeaksDataset_wo_bias(Dataset):
+    """Peaks and background sequences for main model training"""
+
+    def __init__(self, path_sequences_peaks: str, path_sequences_back: str, paths_ATAC_peaks: list, paths_ATAC_back: list, chr_include: list):
+        """
+        Arguments:
+            path_sequences_peaks (string): Path to the pickle file with peaks regions sequences
+            path_sequences_back (string): Path to the pickle file with background regions sequences
+            paths_ATAC_peaks (list of strings): Paths to the pickle files of ATAC tracks and corresponding seq_id, c_type, and if track is_defined
+            paths_ATAC_back (list of strings): Paths to the pickle files of ATAC tracks and corresponding seq_id, c_type, and if track is_defined
+            chr_include (list of string): only keep the sequences on the provided chromosome, used to define train/split
+        """
+
+        #Load peak ATAC tracks
+        self.sequences, self.sequences_id, self.positions = self.load_sequences(path_sequences_peaks, chr_include)
+        self.ATAC_track, self.is_defined, self.idx_seq, self.c_type = self.load_ATAC_tracks(paths_ATAC_peaks, chr_include)
+        
+        #Load the background ATAC tracks
+        sequences, sequences_id, positions = self.load_sequences(path_sequences_back, chr_include)
+        
+        #Add max seq_id of peaks so that it is unique
+        max_seq_id = np.max(self.sequences_id) + 1
+        sequences_id = sequences_id + max_seq_id
+
+        self.sequences = torch.cat((self.sequences, sequences), 0); self.sequences_id = np.concatenate((self.sequences_id, sequences_id), 0)
+        self.positions = np.concatenate((self.positions, positions), 0)
+        
+        ATAC_track, is_defined, idx_seq, c_type = self.load_ATAC_tracks(paths_ATAC_back, chr_include)
+
+        #Add max seq_id of peaks so that it is unique
+        idx_seq = idx_seq + max_seq_id
+
+        self.ATAC_track = torch.cat((self.ATAC_track, ATAC_track), 0); self.is_defined = torch.cat((self.is_defined, is_defined), 0); self.idx_seq = torch.cat((self.idx_seq, idx_seq), 0); self.c_type = np.concatenate((self.c_type, c_type), 0)
+
+        #Define order of c_type for encoding
+        self.unique_c_type = np.sort(np.unique(self.c_type))
+
+    def __len__(self):
+        return self.ATAC_track.shape[0]
+    
+    def load_sequences(self, path_sequence, chr_include):
+
+        #Open sequences files
+        with open(path_sequence, 'rb') as file:
+            sequences = pickle.load(file)
+
+        #Reset index to be integer
+        sequences.reset_index(drop=True, inplace=True)
+        
+        #Only keep sequences from provided chromosomes
+        sequences = sequences[sequences.chr.isin(chr_include)]
+        
+        #Keep chr + pos dataframe
+        sequences.rename(columns={"middle_peak": "middle"}, inplace=True)
+
+        positions =  sequences.loc[:,["chr", "middle"]]
+        positions = positions.to_numpy()
+
+        #Encode sequences
+        sequences = sequences.sequence
+        self.len_seq = len(sequences.iloc[0])
+        sequences = sequences.apply(lambda x: one_hot_encode(x))
+
+        #Store in tensor for faster access
+        sequences_id = sequences.index.to_numpy()
+        sequences = torch.from_numpy(np.stack(sequences.values))
+        sequences = sequences.permute(0,2,1)
+
+        return sequences, sequences_id, positions
+
+    def load_ATAC_tracks(self, paths_ATAC_track:list, chr_include:list):
+        
+        #Define which region are use in split
+        with open(paths_ATAC_track[0], 'rb') as file:
+            chr_track = pd.Series(pickle.load(file))
+        keep_track = chr_track.isin(chr_include)
+
+        with open(paths_ATAC_track[1], 'rb') as file:
+           ATAC_track = pickle.load(file)
+        ATAC_track = ATAC_track[keep_track,:,:]
+        
+        with open(paths_ATAC_track[2], 'rb') as file:
+            is_defined = pickle.load(file)
+        is_defined = is_defined[keep_track,:]
+
+        with open(paths_ATAC_track[3], 'rb') as file:
+            idx_seq = pickle.load(file)
+        idx_seq = idx_seq[keep_track]
+
+        with open(paths_ATAC_track[4], 'rb') as file:
+            c_type = pickle.load(file)
+        c_type = c_type[keep_track]
+
+        return ATAC_track, is_defined, idx_seq, c_type
+
+    def __getitem__(self, idx):
+
+        #Get track and associated encoded sequence input
+        tracks = self.ATAC_track[idx,:,:]
+        
+        seq_idx = self.idx_seq[idx].item()
+
+        seq_idx = np.where(self.sequences_id == seq_idx)[0]
+        input = self.sequences[seq_idx,:,:]
+
+        #Add cell type token to input
+        #Repeat one-hot encoded cell type so that shape = seq_len x nb_cells
+        c_type = self.c_type[idx]
+        
+        mapping = dict(zip(self.unique_c_type, range(len(self.unique_c_type))))    
+        c_type = mapping[c_type]
+        c_type = torch.from_numpy(np.eye(len(self.unique_c_type))[c_type])
+
+        c_type = c_type.tile((input.shape[-1],1)).permute(1,0)[:,:]
+        input = torch.cat((input.squeeze(), c_type), dim=0)
+
+        #Get which tracks should be omitted for the loss computation
+        is_defined = self.is_defined[idx, :]
+
+        return input, tracks, is_defined
+
+
 
