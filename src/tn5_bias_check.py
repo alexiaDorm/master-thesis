3d6db99df93b413f6b45868321e126854d68b4dc
Alexia Dormann <alexia.dormann@epfl.ch>
2024-08-01 08:29:52 +0200

    Update tn5_bias_check.py

--- src/tn5_bias_check.py
+++ src/tn5_bias_check.py
@@ -1,7 +1,9 @@
 import torch
+import numpy as np
+import subprocess
 
 from interpretation.synthetic_seq_analysis import generate_seq_tn5
-from interpretation.interpret import compute_importance_score_c_type, compute_importance_score_bias, visualize_sequence_imp
+from interpretation.interpret import compute_importance_score_wobias, compute_importance_score_bias, visualize_sequence_imp
 from models.models import CATAC_wo_bias, CATAC_w_bias
 
 all_c_type = ['Immature', 'Mesenchymal', 'Myoblast', 'Myogenic', 'Neuroblast',
@@ -14,14 +16,18 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 bias_motifs = [["tn5_1","GCACAGTACAGAGCTG"],["tn5_2","GTGCACAGTTCTAGAGTGTGCAG"],["tn5_3","CCTCTACACTGTGCAGAA"],["tn5_4","GCACAGTTCTAGACTGTGCAG"],["tn5_5","CTGCACAGTGTAGAGTTGTGC"]]
 
 #Generate random sequence with tn5 bias 
+#--------------------------------------------
 seq = []
-for i in range(100):
+for i in range(200):
     for j in range(len(bias_motifs)):
         seq.append(generate_seq_tn5(0.41, 4096, bias_motifs[j][1]))
 
+#Load models
+#--------------------------------------------
+
 #Load the model - WITH tn5 bias
 path_model_bias = '../results/train_res/128_model.pkl'
-model_bias = CATAC_w_bias(nb_conv=8, nb_filters=64, first_kernel=21, 
+model_bias = CATAC_w_bias(nb_conv=8, nb_filters=128, first_kernel=21, 
                       rest_kernel=3, out_pred_len=1024, 
                       nb_pred=4)
         
@@ -30,11 +36,39 @@ model_bias.load_state_dict(torch.load(path_model_bias, map_location=torch.device
 path_model_bias = "../data/Tn5_NN_model.h5"
 
 #Load the model - WITHOUT tn5 bias
-path_model_wobias = '../results/train_res/126_wobias_model.pkl'
-model_wobias = CATAC_wo_bias(nb_conv=8, nb_filters=64, first_kernel=21, 
+path_model_wobias = '../results/train_res/128_wobias_model.pkl'
+model_wobias = CATAC_wo_bias(nb_conv=8, nb_filters=128, first_kernel=21, 
                       rest_kernel=3, out_pred_len=1024, 
                       nb_pred=4)
         
 model_wobias.load_state_dict(torch.load(path_model_wobias, map_location=torch.device('cpu')))
 
-#Compute contribution scores
+#Compute attribution scores
+#--------------------------------------------
+
+#Bias
+seq_bias, scores_bias, proj_score_bias = compute_importance_score_bias(model_bias, path_model_bias, seq, device, "Myogenic", all_c_type, 2)
+np.savez('../results/encod_seq_bias.npz', seq_bias[:,:4,:])
+np.savez('../results/seq_scores_bias.npz', scores_bias[:,:4,:], proj_score_bias[:,:4,:])
+
+#NO Bias
+seq_wobias, scores_wobias, proj_score_wobias = compute_importance_score_wobias(model_bias, path_model_bias, seq, device, "Myogenic", all_c_type, 2)
+np.savez('../results/encod_seq_wobias.npz', seq_wobias[:,:4,:])
+np.savez('../results/seq_scores_wobias.npz', scores_wobias[:,:4,:], proj_score_wobias[:,:4,:])
+
+#Run modisco + TOMTOM
+#--------------------------------------------
+
+#Bias
+cmd_modisco = "modisco motifs -s  ../results/encod_seq_bias.npz -a  ../results/seq_scores_bias.npz -n 2000 -o modisco_results_bias.h5"
+subprocess.run(cmd_modisco, shell=True)
+
+cmd_tomtom = "modisco report -i modisco_results_bias.h5 -o report_bias/TOMTOM/ -s report_bias/TOMTOM/ -m ../data/JASPAR_motif.txt"
+subprocess.run(cmd_tomtom, shell=True)
+
+#NO Bias
+cmd_modisco = "modisco motifs -s  ../results/encod_seq_wobias.npz -a  ../results/seq_scores_wobias.npz -n 2000 -o modisco_results_wobias.h5"
+subprocess.run(cmd_modisco, shell=True)
+
+cmd_tomtom = "modisco report -i modisco_results_wobias.h5 -o report_bias/TOMTOM/ -s report_bias/TOMTOM/ -m ../data/JASPAR_motif.txt"
+subprocess.run(cmd_tomtom, shell=True)
