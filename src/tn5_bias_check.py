e529d96cec31f9fd95129ed03196a87a37a72c90
Alexia Dormann <alexia.dormann@epfl.ch>
2024-08-07 17:32:45 +0200

    Update tn5_bias_check.py

--- src/tn5_bias_check.py
+++ src/tn5_bias_check.py
@@ -66,12 +66,12 @@ np.savez('../results/seq_scores_wobias.npz', scores_wobias[:,:4,:], proj_score_w
 cmd_modisco = "modisco motifs -s  ../results/encod_seq_bias.npz -a  ../results/seq_scores_bias.npz -n 2000 -o modisco_results_bias.h5"
 subprocess.run(cmd_modisco, shell=True)
 
-cmd_tomtom = "modisco report -i modisco_results_bias.h5 -o  -s  -m ../data/JASPAR_motif.txt"
+cmd_tomtom = "modisco report -i modisco_results_bias.h5 -o . -s . -m ../data/JASPAR_motif.txt"
 subprocess.run(cmd_tomtom, shell=True)
 
 """ #NO Bias
 cmd_modisco = "modisco motifs -s  ../results/encod_seq_wobias.npz -a  ../results/seq_scores_wobias.npz -n 2000 -o modisco_results_wobias.h5"
 subprocess.run(cmd_modisco, shell=True)
 
-cmd_tomtom = "modisco report -i modisco_results_wobias.h5 -o  -s  -m ../data/JASPAR_motif.txt"
+cmd_tomtom = "modisco report -i modisco_results_wobias.h5 -o . -s . -m ../data/JASPAR_motif.txt"
 subprocess.run(cmd_tomtom, shell=True) """
