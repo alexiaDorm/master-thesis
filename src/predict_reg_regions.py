1b9029bd05440e46ad5c9399209ec730b220f133
Alexia Dormann <alexia.dormann@epfl.ch>
2024-08-20 16:49:07 +0200

    Update predict_reg_regions.py

--- src/predict_reg_regions.py
+++ src/predict_reg_regions.py
@@ -105,7 +105,7 @@ with open("../results/idx_seq.pkl", 'rb') as file:
 with open("../results/c_type_track.pkl", 'rb') as file:
     c_type = pickle.load(file)
 
-print(idx_seq.isin(active_enhancer.index).shape)
+print(torch.isin(idx_seq, active_enhancer.index).shape)
 print(c_type.shape)
 
 seq_idx = np.where(np.logical_and(idx_seq.isin(active_enhancer.index), c_type == "Myogenic"))[0]
