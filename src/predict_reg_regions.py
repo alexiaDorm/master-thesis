bca5712457d1d4d0a30da4d87d23af04f08c222b
Alexia Dormann <alexia.dormann@epfl.ch>
2024-08-20 17:18:35 +0200

    Update predict_reg_regions.py

--- src/predict_reg_regions.py
+++ src/predict_reg_regions.py
@@ -105,10 +105,10 @@ with open("../results/idx_seq.pkl", 'rb') as file:
 with open("../results/c_type_track.pkl", 'rb') as file:
     c_type = pickle.load(file)
 
-print(torch.isin(idx_seq, torch.from_numpy(active_enhancer.index.to_numpy())).shape)
+print(np.isin(np.array(idx_seq), active_enhancer.index.to_numpy()).shape)
 print(c_type.shape)
 
-seq_idx = np.where(np.logical_and(idx_seq.isin(active_enhancer.index), c_type == "Myogenic"))[0]
+seq_idx = np.where(np.logical_and(np.array(idx_seq).isin(active_enhancer.index), c_type == "Myogenic"))[0]
 ATAC_track = ATAC_track[seq_idx]
 
 with open('../results/target_active_enhancer.pkl', 'wb') as file:
