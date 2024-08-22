7e4344da5f4440f8463e58994b3adcbbb3bbefc8
Alexia Dormann <alexia.dormann@epfl.ch>
2024-08-22 17:06:34 +0200

    Update predict_reg_regions.py

--- src/predict_reg_regions.py
+++ src/predict_reg_regions.py
@@ -36,202 +36,204 @@ model.load_state_dict(torch.load(path_model, map_location=torch.device('cpu')))
 path_model_bias = "../data/Tn5_NN_model.h5"
 model_bias = load_model(path_model_bias)
 
-#Intersection between variants in active enhancers regions and peak set of dataset
-#---------------------------------
+regions_type = ['active_enchancer', 'rep_enhancer', 'promoter']
+bed_files = ['vars_in_acitive_enchancers.bed', 'vars_in_repressed_enchancers.bed', 'variants_in_active_promoters.bed']
 
-#Create peak set bed file
-with open('../results/peaks_seq.pkl', 'rb') as file:
-    peaks = pickle.load(file)
+for h,r in enumerate(regions_type):
 
-peaks = peaks[peaks.chr.str.isnumeric()].iloc[:,:3]
+    #Intersection between variants in active enhancers regions and peak set of dataset
+    #---------------------------------
 
-peaks.to_csv('../results/peaks_set.bed', header=False, index=False, sep='\t')
+    #Create peak set bed file
+    with open('../results/peaks_seq.pkl', 'rb') as file:
+        peaks = pickle.load(file)
 
-#Intersect the variants in active enhancer with the peak set 
-#Change here the file with variants
-cmd_bed = "bedtools intersect -a ../results/peaks_set.bed -b ../data/vars_in_acitive_enchancers.bed -wa -wb > ../results/aevp_pverlap.bed"
-subprocess.run(cmd_bed, shell=True)
+    peaks = peaks[peaks.chr.str.isnumeric()].iloc[:,:3]
 
-active_enhancer = pd.read_csv("../results/aevp_pverlap.bed", header=None, sep='\t', names=["chr_peak", "start_peak", "end_peak", "chr_var", "pos_var", "end_var"])
+    peaks.to_csv('../results/peaks_set.bed', header=False, index=False, sep='\t')
 
-#Add sequence of peak
-with open('../results/peaks_seq.pkl', 'rb') as file:
-    peaks = pickle.load(file)
-active_enhancer.index = active_enhancer.chr_peak.astype(str) + ":" + active_enhancer.start_peak.astype(str) + "-" + active_enhancer.end_peak.astype(str)
-active_enhancer["sequence"] = peaks.loc[active_enhancer.index].sequence
-active_enhancer["middle"] = peaks.loc[active_enhancer.index].middle_peak
+    #Intersect the variants in active enhancer with the peak set 
+    #Change here the file with variants
+    cmd_bed = "bedtools intersect -a ../results/peaks_set.bed -b ../data/"+ bed_files[h] + " -wa -wb > ../results/variants/" + r + "_overlap.bed"
+    subprocess.run(cmd_bed, shell=True)
 
-### Get reference sequenece and mutated sequences for overlapping variants
-#---------------------------------
+    active_enhancer = pd.read_csv("../results/variants/" + r + "_overlap.bed", header=None, sep='\t', names=["chr_peak", "start_peak", "end_peak", "chr_var", "pos_var", "end_var"])
 
-variants = pd.DataFrame(pd.read_csv("../data/Muscle_Skeletal.var.txt", sep='\t').variant_id)
-variants = variants['variant_id'].str.split('_')
+    #Add sequence of peak
+    with open('../results/peaks_seq.pkl', 'rb') as file:
+        peaks = pickle.load(file)
+    active_enhancer.index = active_enhancer.chr_peak.astype(str) + ":" + active_enhancer.start_peak.astype(str) + "-" + active_enhancer.end_peak.astype(str)
+    active_enhancer["sequence"] = peaks.loc[active_enhancer.index].sequence
+    active_enhancer["middle"] = peaks.loc[active_enhancer.index].middle_peak
 
-variants = pd.DataFrame(variants.tolist(), index= variants.index, columns=['chr_var','pos_var', 'ref', 'alt', '_']).iloc[:,:-1]
-variants.chr_var = [idx[3:] for idx in variants.chr_var]
-variants.index = variants.chr_var.astype(str) + ":" + variants.pos_var.astype(str)
+    ### Get reference sequenece and mutated sequences for overlapping variants
+    #---------------------------------
 
-#Join both dataset 
-active_enhancer.index = active_enhancer.chr_var.astype(str) + ":" + active_enhancer.pos_var.astype(str)
+    variants = pd.DataFrame(pd.read_csv("../data/Muscle_Skeletal.var.txt", sep='\t').variant_id)
+    variants = variants['variant_id'].str.split('_')
 
-variants = variants.loc[active_enhancer.index]
-variants.chr_var = variants.chr_var.astype('int64'); variants.pos_var = variants.pos_var.astype('int64')
+    variants = pd.DataFrame(variants.tolist(), index= variants.index, columns=['chr_var','pos_var', 'ref', 'alt', '_']).iloc[:,:-1]
+    variants.chr_var = [idx[3:] for idx in variants.chr_var]
+    variants.index = variants.chr_var.astype(str) + ":" + variants.pos_var.astype(str)
 
-active_enhancer = pd.merge(variants, active_enhancer)
-active_enhancer.index = active_enhancer.chr_var.astype(str) + ":" + active_enhancer.pos_var.astype(str) + "_" + active_enhancer.ref.astype(str) + "_" + active_enhancer.alt.astype(str)
-active_enhancer = active_enhancer[~active_enhancer.index.duplicated(keep='first')]
+    #Join both dataset 
+    active_enhancer.index = active_enhancer.chr_var.astype(str) + ":" + active_enhancer.pos_var.astype(str)
 
-#Create alt sequences
-active_enhancer['dist_var_middle'] = active_enhancer.pos_var - active_enhancer.middle 
-active_enhancer['var_pos_seq'] = 2048 + active_enhancer.dist_var_middle -1 
+    variants = variants.loc[active_enhancer.index]
+    variants.chr_var = variants.chr_var.astype('int64'); variants.pos_var = variants.pos_var.astype('int64')
 
-#check ref match to sequence
-active_enhancer['nucleotide_var'] = [x[active_enhancer.var_pos_seq[i]] for i,x in enumerate(active_enhancer.sequence)]
+    active_enhancer = pd.merge(variants, active_enhancer)
+    active_enhancer.index = active_enhancer.chr_var.astype(str) + ":" + active_enhancer.pos_var.astype(str) + "_" + active_enhancer.ref.astype(str) + "_" + active_enhancer.alt.astype(str)
+    active_enhancer = active_enhancer[~active_enhancer.index.duplicated(keep='first')]
 
-#Create alt sequence
-active_enhancer['sequence_alt'] = [x[:active_enhancer.var_pos_seq[i]] + active_enhancer.alt[i] + x[active_enhancer.var_pos_seq[i]+1:] for i,x in enumerate(active_enhancer.sequence)]
+    #Create alt sequences
+    active_enhancer['dist_var_middle'] = active_enhancer.pos_var - active_enhancer.middle 
+    active_enhancer['var_pos_seq'] = 2048 + active_enhancer.dist_var_middle -1 
 
-#if alt more than one nucleotide, crop sequence alt
-active_enhancer['sequence_alt'] = [x[math.floor((len(x)-4096)/2):-math.ceil((len(x)-4096)/2)] if len(x) > 4096 else x for x in active_enhancer.sequence_alt]
+    #check ref match to sequence
+    active_enhancer['nucleotide_var'] = [x[active_enhancer.var_pos_seq[i]] for i,x in enumerate(active_enhancer.sequence)]
 
-with open('../results/active_enhancer.pkl', 'wb') as file:
-    pickle.dump(active_enhancer, file)
+    #Create alt sequence
+    active_enhancer['sequence_alt'] = [x[:active_enhancer.var_pos_seq[i]] + active_enhancer.alt[i] + x[active_enhancer.var_pos_seq[i]+1:] for i,x in enumerate(active_enhancer.sequence)]
 
-#Find motifs in sequences
-#---------------------------------
-#Create bed of regions 
-reg_regions = active_enhancer[["chr_peak",'start_peak','end_peak']]
-reg_regions.chr_peak = ["chr" + str(x) for x in reg_regions.chr_peak]
-reg_regions.to_csv('../results/reg_regions.bed', header=False, index=False, sep='\t')
+    #if alt more than one nucleotide, crop sequence alt
+    active_enhancer['sequence_alt'] = [x[math.floor((len(x)-4096)/2):-math.ceil((len(x)-4096)/2)] if len(x) > 4096 else x for x in active_enhancer.sequence_alt]
 
-""" cmd = "motifscan scan -i ../results/reg_regions.bed -g hg38 -m motifs_JASPAR -o ../results/rep_enhancer/"
-subprocess.run(cmd, shell=True) """
+    with open('../results/variants/' + r + '.pkl', 'wb') as file:
+        pickle.dump(active_enhancer, file)
 
-cmd = "~/work/jaspar_TBS/bin/extract_TFBSs_JASPAR.sh \
-  -i ~/work/master-thesis/results/reg_regions.bed \
-  -b ~/work/master-thesis//data/JASPAR2024_hg38.bb.1 \
-  -o ~/work/master-thesis//results/active_enhancer/"
+    #Find motifs in sequences
+    #---------------------------------
+    #Create bed of regions 
+    reg_regions = active_enhancer[["chr_peak",'start_peak','end_peak']]
+    reg_regions.chr_peak = ["chr" + str(x) for x in reg_regions.chr_peak]
+    reg_regions.to_csv('../results/reg_regions.bed', header=False, index=False, sep='\t')
 
-subprocess.run(cmd, shell=True)
+    cmd = "~/work/jaspar_TBS/bin/extract_TFBSs_JASPAR.sh \
+    -i ~/work/master-thesis/results/reg_regions.bed \
+    -b ~/work/master-thesis/data/JASPAR2024_hg38.bb.1"
 
-#Get and store the ground truth for each peak investigated for cell type
-#---------------------------------
-""" TIME_POINT = ["D8", "D12", "D20", "D22-15"]
-tmp_reg = active_enhancer.rename(columns={"chr_peak": "chr", "start_peak": "start", "end_peak": "end"})
+    subprocess.run(cmd, shell=True)
 
-all_ATAC = []
-for t in TIME_POINT: 
-    bw_files = '../results/bam_cell_type/' + t +'/' + 'Myogenic'  + '_unstranded.bw'
+    cmd_rename = "mv ../results/extraction_results.bed ../results/variants/" + r + "_motifs.bed"
+    subprocess.run(cmd_rename, shell=True)
 
-    #Get insertion count
-    bw = pyBigWig.open(bw_files)
-    ATAC_tracks = tmp_reg.apply(lambda x: get_continuous_wh_window(bw, x, 0, seq_len=1024), axis=1)
-    ATAC_tracks = np.stack(ATAC_tracks)
-    
-    all_ATAC.append(ATAC_tracks)
+    #Get and store the ground truth for each peak investigated for cell type
+    #---------------------------------
+    TIME_POINT = ["D8", "D12", "D20", "D22-15"]
+    tmp_reg = active_enhancer.rename(columns={"chr_peak": "chr", "start_peak": "start", "end_peak": "end"})
 
-all_ATAC = torch.from_numpy(np.stack(all_ATAC, axis=2))
+    all_ATAC = []
+    for t in TIME_POINT: 
+        bw_files = '../results/bam_cell_type/' + t +'/' + 'Myogenic'  + '_unstranded.bw'
 
-with open('../results/target_promoter.pkl', 'wb') as file:
-    pickle.dump(all_ATAC, file)
+        #Get insertion count
+        bw = pyBigWig.open(bw_files)
+        ATAC_tracks = tmp_reg.apply(lambda x: get_continuous_wh_window(bw, x, 0, seq_len=1024), axis=1)
+        ATAC_tracks = np.stack(ATAC_tracks)
+        
+        all_ATAC.append(ATAC_tracks)
 
-#Predict for variant the ref and alt sequence
-#---------------------------------
-#Define sequences
-ref_seq = active_enhancer.sequence
-alt_seq = active_enhancer.sequence_alt
+    all_ATAC = torch.from_numpy(np.stack(all_ATAC, axis=2))
 
-#Predict tn5 bias for each sequence
-tn5_bias_ref = ref_seq.apply(lambda x: compute_tn5_bias(model_bias, x))
-tn5_bias_alt = alt_seq.apply(lambda x: compute_tn5_bias(model_bias, x))
+    with open('../results/variants/target_'+ r + '.pkl', 'wb') as file:
+        pickle.dump(all_ATAC, file)
 
-#On-hot encode the sequences
-ref_seq_enc = ref_seq.apply(lambda x: one_hot_encode(x))
-alt_seq_enc = alt_seq.apply(lambda x: one_hot_encode(x))
+    #Predict for variant the ref and alt sequence
+    #---------------------------------
+    #Define sequences
+    ref_seq = active_enhancer.sequence
+    alt_seq = active_enhancer.sequence_alt
 
-#Add cell type encoding
-c_type = "Myogenic"
-mapping = dict(zip(all_c_type, range(len(all_c_type))))    
-c_type = mapping[c_type]
-c_type = torch.from_numpy(np.eye(len(all_c_type), dtype=np.float32)[c_type])
-c_type = c_type.tile((ref_seq_enc[0].shape[0],1))
+    #Predict tn5 bias for each sequence
+    tn5_bias_ref = ref_seq.apply(lambda x: compute_tn5_bias(model_bias, x))
+    tn5_bias_alt = alt_seq.apply(lambda x: compute_tn5_bias(model_bias, x))
 
-ref_seq_enc = [np.concatenate((s,c_type), axis=1) for s in ref_seq_enc]
-ref_seq_enc = torch.tensor(ref_seq_enc).permute(0,2,1)
+    #On-hot encode the sequences
+    ref_seq_enc = ref_seq.apply(lambda x: one_hot_encode(x))
+    alt_seq_enc = alt_seq.apply(lambda x: one_hot_encode(x))
 
-alt_seq_enc = [np.concatenate((s,c_type), axis=1) for s in alt_seq_enc]
-alt_seq_enc = torch.tensor(alt_seq_enc).permute(0,2,1)
+    #Add cell type encoding
+    c_type = "Myogenic"
+    mapping = dict(zip(all_c_type, range(len(all_c_type))))    
+    c_type = mapping[c_type]
+    c_type = torch.from_numpy(np.eye(len(all_c_type), dtype=np.float32)[c_type])
+    c_type = c_type.tile((ref_seq_enc[0].shape[0],1))
 
-#Predict
-x_ref, profile_ref, count_ref = model(ref_seq_enc, torch.tensor(np.vstack(tn5_bias_ref)))
-x_alt, profile_alt, count_alt = model(alt_seq_enc, torch.tensor(np.vstack(tn5_bias_alt)))
+    ref_seq_enc = [np.concatenate((s,c_type), axis=1) for s in ref_seq_enc]
+    ref_seq_enc = torch.tensor(ref_seq_enc).permute(0,2,1)
 
-with open('../results/pred_profile_promoter.pkl', 'wb') as file:
-    pickle.dump(profile_ref, file)
+    alt_seq_enc = [np.concatenate((s,c_type), axis=1) for s in alt_seq_enc]
+    alt_seq_enc = torch.tensor(alt_seq_enc).permute(0,2,1)
 
-with open('../results/pred_count_promoter.pkl', 'wb') as file:
-    pickle.dump(count_ref, file)
+    #Predict
+    x_ref, profile_ref, count_ref = model(ref_seq_enc, torch.tensor(np.vstack(tn5_bias_ref)))
+    x_alt, profile_alt, count_alt = model(alt_seq_enc, torch.tensor(np.vstack(tn5_bias_alt)))
 
-with open('../results/alt_pred_profile_promoter.pkl', 'wb') as file:
-    pickle.dump(profile_alt, file)
+    with open('../results/variants/pred_profile_'+ r + '.pkl', 'wb') as file:
+        pickle.dump(profile_ref, file)
 
-with open('../results/alt_pred_count_promoter.pkl', 'wb') as file:
-    pickle.dump(count_alt, file)
+    with open('../results/variants/pred_count_'+ r + '.pkl', 'wb') as file:
+        pickle.dump(count_ref, file)
 
-#Load predictions
-#---------------------------------
-import pickle
-import torch
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.spatial import distance
+    with open('../results/variants/alt_pred_profile_'+ r + '.pkl', 'wb') as file:
+        pickle.dump(profile_alt, file)
 
-with open("../results/pred_profile_promoter.pkl", 'rb') as file:
-    profile = pickle.load(file)
-with open("../results/pred_count_promoter.pkl", 'rb') as file:
-    count = pickle.load(file)
+    with open('../results/variants/alt_pred_count_'+ r + '.pkl', 'wb') as file:
+        pickle.dump(count_alt, file)
 
-with open("../results/alt_pred_profile_promoter.pkl", 'rb') as file:
-    alt_profile = pickle.load(file)
-with open("../results/alt_pred_count_promoter.pkl", 'rb') as file:
-    alt_count = pickle.load(file)    
+    #Load predictions
+    #---------------------------------
+    import pickle
+    import torch
+    import matplotlib.pyplot as plt
+    import numpy as np
+    from scipy.spatial import distance
 
-profile = torch.nn.functional.softmax(profile, dim=1).detach().numpy()
-alt_profile = torch.nn.functional.softmax(alt_profile, dim=1).detach().numpy()
+    with open("../results/variants/pred_profile_"+ r + ".pkl", 'rb') as file:
+        profile = pickle.load(file)
+    with open("../results/variants/pred_count_"+ r + ".pkl", 'rb') as file:
+        count = pickle.load(file)
 
+    with open("../results/variants/alt_pred_profile_"+ r + ".pkl", 'rb') as file:
+        alt_profile = pickle.load(file)
+    with open("../results/variants/alt_pred_count_"+ r + ".pkl", 'rb') as file:
+        alt_count = pickle.load(file)    
 
-#Identify the variants significantly modifying the total count predictions
-#---------------------------------
-i, t = 50, 3
-jsds, diffs, which_seq = [], [], []
-for i in range (count.size(0)):
-    for t in range(4):
-        jsd = distance.jensenshannon(profile[i,t,:], alt_profile[i,t,:])
-        jsds.append(jsd)
+    profile = torch.nn.functional.softmax(profile, dim=1).detach().numpy()
+    alt_profile = torch.nn.functional.softmax(alt_profile, dim=1).detach().numpy()
 
-        diff =  alt_count[i,t].detach().numpy() - count[i,t].detach().numpy()
-        diffs.append(diff)
+    #Identify the variants significantly modifying the total count predictions
+    #---------------------------------
+    jsds, diffs, which_seq = [], [], []
+    for i in range (count.size(0)):
+        for t in range(4):
+            jsd = distance.jensenshannon(profile[i,t,:], alt_profile[i,t,:])
+            jsds.append(jsd)
 
-        if abs(diff) > 0.25:
-            which_seq.append(i)
+            diff =  alt_count[i,t].detach().numpy() - count[i,t].detach().numpy()
+            diffs.append(diff)
 
-which_seq = np.unique(which_seq)
+            if diff > 0.17661169 or diff < -0.20696962:
+                which_seq.append(i)
 
-#Compute attributions maps for identified sequeneces
-#---------------------------------
-from interpretation.interpret import compute_importance_score_bias
+    which_seq = np.unique(which_seq)
 
-active_enhancer = active_enhancer.iloc[which_seq]
+    #Compute attributions maps for identified sequeneces
+    #---------------------------------
+    from interpretation.interpret import compute_importance_score_bias
 
-#Define sequences
-ref_seq = active_enhancer.sequence
-alt_seq = active_enhancer.sequence_alt
+    active_enhancer = active_enhancer.iloc[which_seq]
 
-#Compute attribution scores
-seq, shap_scores, proj_scores = compute_importance_score_bias(model, path_model_bias, ref_seq, device, "Myogenic", all_c_type, 1)
-seq_alt, shap_scores_alt, proj_scores_alt = compute_importance_score_bias(model, path_model_bias, alt_seq, device, "Myogenic", all_c_type, 1)
+    #Define sequences
+    ref_seq = active_enhancer.sequence
+    alt_seq = active_enhancer.sequence_alt
 
-with open('../results/promoter_proj_scores.pkl', 'wb') as file:
-    pickle.dump(proj_scores, file)
-with open('../results/promoter_proj_scores_alt.pkl', 'wb') as file:
-    pickle.dump(proj_scores_alt, file) """
+    #Compute attribution scores
+    seq, shap_scores, proj_scores = compute_importance_score_bias(model, path_model_bias, ref_seq, device, "Myogenic", all_c_type, 1)
+    seq_alt, shap_scores_alt, proj_scores_alt = compute_importance_score_bias(model, path_model_bias, alt_seq, device, "Myogenic", all_c_type, 1)
+
+    with open('../results/variants/'+ r + '_proj_scores.pkl', 'wb') as file:
+        pickle.dump(proj_scores, file)
+    with open('../results/variants/'+ r + '_proj_scores_alt.pkl', 'wb') as file:
+        pickle.dump(proj_scores_alt, file)
