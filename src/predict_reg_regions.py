ffa59b7b09870e0f22e28d7aa998ec64f212dd21
Alexia Dormann <alexia.dormann@epfl.ch>
2024-08-23 15:39:46 +0200

    Update predict_reg_regions.py

--- src/predict_reg_regions.py
+++ src/predict_reg_regions.py
@@ -113,13 +113,9 @@ for h,r in enumerate(regions_type):
     -i ~/work/master-thesis/results/reg_regions.bed \
     -b ~/work/master-thesis/data/JASPAR2024_hg38.bb.1 \
     -o ~/work/master-thesis/results/variants/" + r + "_motifs.bed"
-
     subprocess.run(cmd, shell=True)
 
-    cmd_rename = "mv ../results/extraction_results.bed ../results/variants/" + r + "_motifs.bed"
-    #subprocess.run(cmd_rename, shell=True)
-
-    """ #Get and store the ground truth for each peak investigated for cell type
+    #Get and store the ground truth for each peak investigated for cell type
     #---------------------------------
     TIME_POINT = ["D8", "D12", "D20", "D22-15"]
     tmp_reg = active_enhancer.rename(columns={"chr_peak": "chr", "start_peak": "start", "end_peak": "end"})
@@ -225,6 +221,8 @@ for h,r in enumerate(regions_type):
     from interpretation.interpret import compute_importance_score_bias
 
     active_enhancer = active_enhancer.iloc[which_seq]
+    with open('../results/variants/' + r + '_diff_var.pkl', 'wb') as file:
+        pickle.dump(active_enhancer, file)
 
     #Define sequences
     ref_seq = active_enhancer.sequence
@@ -237,4 +235,4 @@ for h,r in enumerate(regions_type):
     with open('../results/variants/'+ r + '_proj_scores.pkl', 'wb') as file:
         pickle.dump(proj_scores, file)
     with open('../results/variants/'+ r + '_proj_scores_alt.pkl', 'wb') as file:
-        pickle.dump(proj_scores_alt, file) """
+        pickle.dump(proj_scores_alt, file)
